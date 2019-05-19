0000b7b8ee9364967d74699aba22b21957433168
KorablinaElla <43074973+KorablinaElla@users.noreply.github.com>
2019-05-19 22:38:01 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,36 @@
 #include "toguess.h"
 #include <iostream>
+#include <string>
+#include <Windows.h>
 
 using namespace std;
 int main() {
 	setlocale(LC_ALL, "Rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	/*->1.вопрос пользователю
-	    2.обработка ответов, пересчет вероятностей для всех ответов
+		2.обработка ответов, пересчет вероятностей для всех ответов
 		3.проверка возможности дать ответ ( если возможности нет, идём к 1)
 		4.выдача ответа ожидание подтвеждения пользователем,если верный, то идем к 6
 		5.Если ответ неверный, то добавляем новый ответ и персонажа в базу
 		6.Конец игры
 	*/
 	//создаём экземпляр нашей игры
-	toguess tg = toguess();
+	assumption tg = assumption();
+	cout << "Соотношение цифр и ответов:" << endl;
+	cout << "1-да" << endl;
+	cout << "0-нет" << endl;
+	cout << "2-скорее да" << endl;
+	cout << "3-скорее нет" << endl;
 	//начинаем новую игру и подгружаем базу
 	tg.newgame();
 	//начинаем задавать вопросы
-	int q=tg.getQuestion();
+	cout << endl;
+	cout << "Игра началась!" << endl;
+	cout << endl;
+	int q = tg.getQuestion();
 	tg.dellQuestion(q);
-	int p_ans,ans;
+	int p_ans, ans;
 	cin >> p_ans;
 	tg.setQuestion(q, p_ans);
 	ans = tg.getResponse();
@@ -29,18 +41,21 @@ int main() {
 		tg.setQuestion(q, p_ans);
 		ans = tg.getResponse();
 		tg.dellQuestion(q);
-		
 	}
 	//Если ответ угада верно игра завершается
-	cout << "Вы загодали " << tg.getfResponse(ans) << " ?"<<endl;
+	cout << endl;
+	cout << "Соотношение цифр и ответов:" << endl;
 	cout << "1-да" << endl;
 	cout << "0-нет" << endl;
+	cout << endl;
+	cout << "Ваш персонаж - " << tg.getfResponse(ans) << " ?" << endl;
 	cin >> p_ans;
 	//Если нет, то добавляем персонажа в базу
 	if (!ans) {
-		char* response;
+		char* response = new char [2024];
 		cout << "Введите загаданного вами персонажа:" << endl;
 		cin >> response;
+		cout<< response <<endl;
 		tg.newResponse(response);
 		tg.newQuestion(response);
 		tg.save();
